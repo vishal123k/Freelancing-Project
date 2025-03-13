51b3ff6bc54e56d98b61e5f5e5fee19219f683ea
Himanshu <himanshujaglyan29@gmail.com>
2025-03-13 05:16:24 -0700

    AddExpert

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,14 @@ import MostPopular from "../components/MostPopular";
 import TrendingOffers from "../components/TrendingOffers";
 import Footer from "../components/Footer"
 import MainPage from "../components/MainPage";
+import ExpertFreelancer from "../components/ExpertFreelancer"
 
 const Home = () => {
   return (
    <div className="bg-white">
     <MainPage/>
         <MostPopular/>
+        <ExpertFreelancer/>
         <TrendingOffers/>
         <Footer/>
       </div>
