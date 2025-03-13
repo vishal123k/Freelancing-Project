96c1a66fa54f5dfcccad93543852d79c83be1e72
Himanshu <himanshujaglyan29@gmail.com>
2025-03-13 10:04:17 -0700

    ProfileSet

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../components/Banner'
 import ProfileImage from '../components/ProfileImage'
+import About from "../components/About"
 
 const ProfilePage = () => {
   return (
     <div>
         <Banner/>
         <ProfileImage/>
+        <About/>
     </div>
   )
 }
