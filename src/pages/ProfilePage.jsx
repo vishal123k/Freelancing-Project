8cbb033643bb1ddc0e699bd884fa759c89cf8174
Himanshu <himanshujaglyan29@gmail.com>
2025-03-13 08:28:59 -0700

    profilePage

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Banner from '../components/Banner'
+import ProfileImage from '../components/ProfileImage'
+
+const ProfilePage = () => {
+  return (
+    <div>
+        <Banner/>
+        <ProfileImage/>
+    </div>
+  )
+}
+
+export default ProfilePage
